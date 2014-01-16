060b55703e899290a536624dcba67081512d2f26
Mark Lester <mc_lester@yahoo.co.uk>
2014-01-16 08:53:18 +0000

    first commit from eclipse

---
+++ Gravity Simulator/src/Force.java
@@ -0,0 +1,15 @@
+public class Force {
+public static double SCALE=1;
+ 	public double force(double distance, double massA, double massB,double combined_radii){
+		return 0;
+	}
+ 	public double potential(double distance, double massA, double massB, double combined_radii){
+		return 0;
+	}
+ 	public double centrifugal(double distance, double massA){
+		return 0;
+	}
+}
+
+
+
