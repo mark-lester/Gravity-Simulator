b9fe6f16918c6019b4f0a02b47c333c95a633797
Mark Lester <mc_lester@yahoo.co.uk>
2014-01-18 04:42:12 +0000

    sqert support

---
+++ Gravity Simulator/src/GravityInverseCube.java
@@ -0,0 +1,14 @@
+public class GravityInverseCube extends Force {
+	
+ 	public double force(double distance, double massA, double massB, double combined_radii){
+ 		distance=Math.max(combined_radii/2, distance);
+		return (SCALE * massA * massB) /  (distance * distance * distance);
+	}
+ 	public double potential(double distance, double massA, double massB,double combined_radii){
+ 		distance=Math.max((double)1, distance);
+		return (SCALE * massA * massB) / (2 * distance * distance);
+	}
+ 	public double centrifugal(double distance, double mass){
+		return Math.sqrt(SCALE*mass/(distance*distance));
+	}
+}
