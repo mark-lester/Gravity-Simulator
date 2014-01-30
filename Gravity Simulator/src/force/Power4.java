f2fda9e6bec4e90f92141d35723a4b2ac9e5d8de
Mark Lester <mc_lester@yahoo.co.uk>
2014-01-30 21:50:20 +0000

    added x^4 force def

---
+++ Gravity Simulator/src/force/Power4.java
@@ -0,0 +1,10 @@
+package force;
+
+public class Power4 extends Force  {
+	public double force(double distance, double massA, double massB, double combined_radii){
+		return massA * massB * distance * distance * distance * distance;
+	}
+ 	public double potential(double distance, double massA, double massB,double combined_radii){
+		return massA * massB * distance * distance *distance *distance *distance /5;
+	}
+}
